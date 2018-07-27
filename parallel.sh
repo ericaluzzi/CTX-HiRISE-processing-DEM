611064a531e1fef44721882c6c721a32d149e41b
Erica Luzzi <e.luzzi@jacobs-university.de>
2018-07-27 15:41:37 +0200

    Add files via upload

---
+++ parallel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+NAME=${line:0:26}
+
+parallel mroctx2isis from={}.IMG to={.}_L0.cub ::: $NAME
+
+parallel ctxcal from={.}_L0.cub to={.}_L1cal.cub ::: $NAME
+
+parallel ctxevenodd from={.}_L1cal.cub to={.}_L1.cub ::: $NAME
+
+parallel spiceinit from={.}_L1.cub ::: $NAME
+
+parallel cam2map from={.}_L1.cub to={.}_L2.cub map=mars_simp0.map ::: $NAME
+
+gdal_translate "$NAME"_L2.cub "$NAME"_L2_simp0.tif
+
+rm "$NAME"_L0.cub
+
+rm "$NAME"_L1cal.cub
+
+rm "$NAME"_L1.cub
+
+rm "$NAME"_L2.cub
+
+done < "$1"
