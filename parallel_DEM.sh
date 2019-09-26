b736741653ef10898c24ef05909072d2932f5e18
Erica Luzzi <e.luzzi@jacobs-university.de>
2019-09-26 14:20:11 +0200

    Update parallel_DEM.sh
    - spiceinit in the right position
    - cam2map4stereo instead of cam2map
    - bundle adjust with a minor minimum of matches

--- parallel_DEM.sh
+++ parallel_DEM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ue
 set -xv
 
@@ -7,9 +8,9 @@ set -xv
 # ------------------------------------------------------------------------------------------------
 
 INPUT_FILE="$1"
-
 declare -a FILES=()
 declare -a NAMES=()
+declare -a FILESL1=()
 declare -a FILESL2=()
 
 OLDIFS="$IFS"
@@ -25,35 +26,38 @@ for ((i=0;i<$NUMLINES;i++)); do
     #NAME=${NAME%.img}
     NAMES[$i]=${NAME}
 
-    FILE_L2="${NAME}_L2.cub"
+    FILE_L2="${NAME}_L1.map.cub"
     FILESL2[$i]="$FILE_L2"
+    FILE_L1="${NAME}_L1.cub"
+    FILESL1[$i]="$FILE_L1"
+
 done
 
 parallel mroctx2isis from={}.IMG to={.}_L0.cub ::: ${NAMES[@]}
+
+parallel spiceinit from={.}_L0.cub ::: ${NAMES[@]}
 
 parallel ctxcal from={.}_L0.cub to={.}_L1cal.cub ::: ${NAMES[@]}
 
 parallel ctxevenodd from={.}_L1cal.cub to={.}_L1.cub ::: ${NAMES[@]}
 
-parallel spiceinit from={.}_L1.cub ::: ${NAMES[@]}
+cam2map4stereo.py ${FILESL1[*]} 
 
-parallel cam2map from={.}_L1.cub to={.}_L2.cub map=map_template.eqc ::: ${NAMES[@]}
-
-bundle_adjust ${FILESL2[*]} -o match_files/match
+bundle_adjust ${FILESL2[*]} --min-matches 1 -o match_files/match
 
 RES_STEREO=$(printf "_%s" "${NAMES[@]}")
 RES_STEREO="${RES_STEREO:1}"
 
-stereo --threads=4 ${FILESL2[*]} \
-    results_ba/$RES_STEREO \
-    --bundle-adjust-prefix match_files/match
+stereo --threads=4 ${FILESL2[*]} --alignment-method none \
+   results_ba/$RES_STEREO \
+   --bundle-adjust-prefix match_files/match
 
 point2dem --t_srs "+proj=eqc +lat_0=0 +lon_0=0 +k=1 +a=3396190 +b=3396190 +units=m +no_defs" \
     --nodata -32767 --dem-hole-fill-len 500 --orthoimage-hole-fill-len 500 --remove-outliers-params 75.0 3.0 \
     --orthoimage results_ba/${RES_STEREO}-L.tif  results_ba/${RES_STEREO}-PC.tif \
     -o ASP-${RES_STEREO}
 
-    
+
 # Cleaning
 
 rm *_L0.cub
@@ -62,42 +66,6 @@ rm *_L1cal.cub
 
 rm *_L1.cub
 
+
 # The end.
-
-
-
-
-
-
-# while IFS='' read -r line || [[ -n "$line" ]]; do
-# 
-#     #NAME=${line%.IMG}
-#     NAME=${line:0:26}
-# 
-#     CUBNAME_i="$NAME"
-#     CUBFILE_i="${NAME}_L2.cub"
-# 
-#     i_=${#CUBFILES[@]}
-#     
-#     CUBFILES[$i_]="$CUBFILE_i"
-#     CUBNAMES[$i_]="$CUBNAME_i"
-# 
-#     
-#     parallel mroctx2isis from={}.IMG to={.}_L0.cub ::: $NAME
-# 
-#     parallel ctxcal from={.}_L0.cub to={.}_L1cal.cub ::: $NAME
-# 
-#     parallel ctxevenodd from={.}_L1cal.cub to={.}_L1.cub ::: $NAME
-# 
-#     parallel spiceinit from={.}_L1.cub ::: $NAME
-# 
-#     parallel cam2map from={.}_L1.cub to="${CUBFILE_i}" map=map_template.eqc ::: $NAME
-# 
-#     rm "$NAME"_L0.cub
-# 
-#     rm "$NAME"_L1cal.cub
-# 
-#     rm "$NAME"_L1.cub
-# 
-# done < "$INPUT_FILE"
-
+                                                                                                                                                                              72,1          Bot
